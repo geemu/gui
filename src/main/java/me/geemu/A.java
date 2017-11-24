8fdfa015074b96fcb8cb80eab3183a8e08bd8e0b
chenfangming <chenfangming@91zdan.com>
2017-11-24 15:01:19 +0800

    占位符

---
+++ src/main/java/me/geemu/A.java
@@ -0,0 +1,10 @@
+package me.geemu;
+
+/**
+ * @author geemu
+ * Email：cfmmail@sina.com
+ * Date：2017/11/24 15:00
+ * Despriction：me.geemu
+ */
+public class A {
+}
---
+++ src/main/java/me/geemu/chapter1/A.java
@@ -0,0 +1,10 @@
+package me.geemu.chapter1;
+
+/**
+ * @author geemu
+ * Email：cfmmail@sina.com
+ * Date：2017/11/24 15:01
+ * Despriction：me.geemu.chapter1
+ */
+public class A {
+}
